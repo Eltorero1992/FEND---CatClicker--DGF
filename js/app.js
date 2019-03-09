a3c6e20ac7e8dbacfcf544f0ca8c498cb7f50324
Daniel Gomez Firmat <DanielGFirmat@gmail.com>
2019-03-09 22:33:44 +0000

    Automated picture clicker

--- js/app.js
+++ js/app.js
@@ -2,12 +2,54 @@
 $(function(){
 
 
-	const catImage = $("img.catImage")
+	let catImage = $("img.catImage");
+	let catNames = ["John","Mike"];
+	let imgSrc = ["img/625069434_db86b67df8_z.jpg", "img/catTwo.jpeg"]
 
-	catImage.on('click', function() {
-		const counter = document.querySelector("div.counter")
-		counter.innerText = Number(counter.innerText) + 1
-	});
+
+for (var i = 0 ; i <= catNames.length-1; i++) {
+
+		const newDiv = document.createElement("div");
+		const newH1  = document.createElement("h1")
+		const newImg = document.createElement("img");
+		const newDivCounter = document.createElement("div");
+		const catTextName = document.createTextNode(catNames[i])
+		const counterInt = document.createTextNode(0);
+
+
+		$('.container').append(newDiv);
+
+		$(newDiv).attr({
+			class: "catContainer" + i
+		})
+
+		newDiv.append(newH1);
+
+		console.log(catNames[i])
+
+		newH1.appendChild(catTextName);
+
+		newDiv.append(newImg);
+
+		$(newImg).attr({
+			class : "cat" + i,
+			src: imgSrc[i]
+		});
+
+		newDiv.append(newDivCounter)
+
+		$(newDivCounter).attr({
+			class: "counter"
+		})
+
+		newDivCounter.append(counterInt);
+
+		$(newImg).on('click', function() {
+			newDivCounter.innerText = Number(newDivCounter.innerText) + 1
+		});
+
+}
+
 
 }());
 
