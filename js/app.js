4ffee7f615024d4eb9f1bc124959026b5549576b
Daniel Gomez Firmat <DanielGFirmat@gmail.com>
2019-03-10 10:19:05 +0000

    Added cat list and display

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
+//eslint development
+
 
 $(function(){
 
-
-	let catImage = $("img.catImage");
-	let catNames = ["John","Mike"];
-	let imgSrc = ["img/625069434_db86b67df8_z.jpg", "img/catTwo.jpeg"]
-
+	let catNames = ["John","Mike", "Abel", "Nico", "Manuel"];
+	let imgSrc = ["img/625069434_db86b67df8_z.jpg", "img/catTwo.jpeg" , "img/2290467335_89067c7b51_z.jpg", "img/catfour.jpeg","img/catfive.jpg"]
 
 for (var i = 0 ; i <= catNames.length-1; i++) {
 
@@ -15,13 +14,28 @@ for (var i = 0 ; i <= catNames.length-1; i++) {
 		const newDivCounter = document.createElement("div");
 		const catTextName = document.createTextNode(catNames[i])
 		const counterInt = document.createTextNode(0);
+		const catList = document.createElement("ul");
+		const catEntry = document.createElement("li");
+		const newA = document.createElement("a");
 
+		if (i === 0) {
+			$('.container').append(catList);
+		}
+
+		$('ul').append(catEntry);
+
+		catEntry.append(newA);
+
+		newA.appendChild(document.createTextNode(catNames[i]));
+
+		$(newA).attr({
+			href: '#' + catNames[i]
+		})
 
 		$('.container').append(newDiv);
 
-		$(newDiv).attr({
-			class: "catContainer" + i
-		})
+		$(newDiv).addClass("catContainer" + i)
+		$(newDiv).addClass("cat-hidden")
 
 		newDiv.append(newH1);
 
@@ -47,6 +61,11 @@ for (var i = 0 ; i <= catNames.length-1; i++) {
 		$(newImg).on('click', function() {
 			newDivCounter.innerText = Number(newDivCounter.innerText) + 1
 		});
+
+		$(newA).on('click', function() {
+			$(newDiv).toggleClass('cat-hidden')
+			console.log($(newDiv))
+		})
 
 }
 
